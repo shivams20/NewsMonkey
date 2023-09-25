d893dd0a9ab285b47c312c37db24438a00687858
shivams20 <109885021+shivams20@users.noreply.github.com>
2023-09-25 17:46:55 +0530

    first commit

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: 0,
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+
+          <img
+            src={
+              !imageUrl
+                ? "https://www.hindustantimes.com/ht-img/img/2023/05/26/1600x900/WhatsApp_Image_2023-05-27_at_005539_1685129246606_1685129246799.jpeg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title} Inbox</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+export default NewsItem;
